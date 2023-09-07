61a4893c5a662b210883d9f2ff37160f1936bf86
RaghaSindhuraK <144393120+RaghaSindhuraK@users.noreply.github.com>
2023-09-07 17:27:55 -0500

    Initial Commit

---
+++ WeatherAppCCUITests/WeatherAppCCUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WeatherAppCCUITestsLaunchTests.swift
+//  WeatherAppCCUITests
+//
+//  Created by Ragha Sindhura Kurella Venkata Subba on 9/7/23.
+//
+
+import XCTest
+
+final class WeatherAppCCUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
