9ea93c51cf881a71d74e9a06029fafda86399be5
RaghaSindhuraK <144393120+RaghaSindhuraK@users.noreply.github.com>
2023-09-08 16:37:20 -0500

    Weather UI updates

--- WeatherAppCC/WeatherSearch/View/WeatherSearchController.swift
+++ WeatherAppCC/WeatherSearch/View/WeatherSearchController.swift
@@ -8,13 +8,49 @@
 import UIKit
 class WeatherSearchController: UIViewController {
     
+    @IBOutlet weak var weatherView: UIStackView!
+    
+    @IBOutlet weak var cityName: UILabel!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBOutlet weak var weatherCondition: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var maxTemp: UILabel!
+    
+    @IBOutlet weak var minTemp: UILabel!
+    
     let weatherSearchViewModel = WeatherSearchViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        weatherView.isHidden = true
+        
         
     }
+    
+    private func configure() {
+        cityName.text = weatherSearchViewModel.cityName
+        temperatureLabel.text = weatherSearchViewModel.temperatureLabel
+        weatherCondition.text = weatherSearchViewModel.weatherCondition
+        maxTemp.text = weatherSearchViewModel.maxTemperature
+        minTemp.text = weatherSearchViewModel.minTemperature
+        weatherView.isHidden = false
+    
+        imageView.loadImage(with: weatherSearchViewModel.imageUrl)
+    }
+    
+    private func handleError() {
+        weatherView.isHidden = true
+        
+        let alert = UIAlertController(title: "Something went wrong", message: "Please enter a valid city name in US or try again later", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true)
+    }
+    
 }
 
 extension WeatherSearchController: UISearchBarDelegate {
@@ -24,13 +60,48 @@ extension WeatherSearchController: UISearchBarDelegate {
         guard let text = searchBar.text,
               text != "" else { return }
         
-        weatherSearchViewModel.getWeatherDetails(with: text) { refreshWeather in
+        weatherSearchViewModel.getWeatherDetails(with: text) { [weak self] refreshWeather in
             guard refreshWeather else {
-                //showAlert()
+                self?.handleError()
                 return
             }
-            //configure()
+            self?.configure()
         }
+    }
+}
+
+extension UIImageView {
+    func loadImage(with urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
+        let cache = URLCache.shared
+        let request = URLRequest(url: url)
+        
+        if let data = cache.cachedResponse(for: request)?.data {
+            self.image = UIImage(data: data)
+        } else {
+            URLSession.shared.dataTask(with: request) { data, urlResponse, error in
+                DispatchQueue.main.async {
+                    guard let responseData = data,
+                          let urlresponse = urlResponse,
+                          error == nil else {
+                        DispatchQueue.main.async {
+                            self.image = nil
+                        }
+                        return
+                    }
+                    
+                    cache.storeCachedResponse(CachedURLResponse(response: urlresponse, data: responseData), for: request)
+                    
+                    DispatchQueue.main.async {
+                        self.image = UIImage(data: responseData)
+                    }
+                }
+            }.resume()
+        }
+            
+        
+        
     }
 }
     
--- WeatherAppCC/WeatherSearch/ViewModel/WeatherSearchViewModel.swift
+++ WeatherAppCC/WeatherSearch/ViewModel/WeatherSearchViewModel.swift
@@ -11,10 +11,38 @@ class WeatherSearchViewModel: NSObject {
     private var weatherSearchDataModel: WeatherSearchDataModel?
     private let weatherSearchDataManager = WeatherSearchDataManager()
     
+    private func convertunits(value: Double) -> Int {
+        Int(((value - 273.15) * 9/5) + 32)
+    }
+    
     func getWeatherDetails(with cityName: String, callback: @escaping (Bool) -> Void) {
         weatherSearchDataManager.getWeatherDetails(with: cityName) { [weak self] model in
             self?.weatherSearchDataModel = model
             callback(model != nil)
         }
+    }
+    
+    var cityName: String? {
+        weatherSearchDataModel?.name
+    }
+    var temperatureLabel: String? {
+        let value = convertunits(value: weatherSearchDataModel?.main.temp ?? 0.0)
+        return "\(value)"
+    }
+    var weatherCondition: String? {
+        "\(weatherSearchDataModel?.weather.first?.main ?? "")"
+    }
+    var maxTemperature: String? {
+        let value = convertunits(value: weatherSearchDataModel?.main.temp_max ?? 0.0)
+        return "H: \(value)"
+    }
+    var minTemperature: String? {
+        let value = convertunits(value: weatherSearchDataModel?.main.temp_min ?? 0.0)
+        return "L: \(value)"
+    }
+    var imageUrl: String {
+        let urlString = "https://openweathermap.org/img/wn/%@@2x.png"
+        guard let imagename = weatherSearchDataModel?.weather.first?.icon else { return "" }
+        return String(format: urlString, imagename)
     }
 }
